b9955401434fc6a0edbade4ea4f1e7bdbe5aba0a
Fagor <oleg-veber@mail.ru>
2023-10-25 16:11:16 +0700

    add swagger documentation

--- src/main/java/ru/nsu/fit/directors/orderservice/OrderServiceApplication.java
+++ src/main/java/ru/nsu/fit/directors/orderservice/OrderServiceApplication.java
@@ -1,11 +1,23 @@
 package ru.nsu.fit.directors.orderservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@OpenAPIDefinition(servers = {
+	@Server(
+		description = "This is the localhost",
+		url = "localhost:8080/order"
+	),
+	@Server(
+		description = "This is the real server",
+		url = "80.87.200.185/order"
+	)
+})
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {

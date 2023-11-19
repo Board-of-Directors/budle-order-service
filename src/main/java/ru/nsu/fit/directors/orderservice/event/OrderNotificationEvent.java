c8138eff59ef5495a58769c6e1b9506315dfcf8c
Fagor <oleg-veber@mail.ru>
2023-11-19 11:28:44 +0700

    made return orderId

--- src/main/java/ru/nsu/fit/directors/orderservice/event/OrderNotificationEvent.java
+++ src/main/java/ru/nsu/fit/directors/orderservice/event/OrderNotificationEvent.java
@@ -1,4 +1,4 @@
 package ru.nsu.fit.directors.orderservice.event;
 
-public record OrderNotificationEvent(String message, Long userId) {
+public record OrderNotificationEvent(String message, Long userId, Long orderId) {
 }

40dc1a517290140544bfd20bd7225e69e7c79885
Fagorym <oleg-veber@mail.ru>
2024-03-27 22:27:10 +0700

    Rename column

--- src/main/java/ru/nsu/fit/directors/orderservice/model/Message.java
+++ src/main/java/ru/nsu/fit/directors/orderservice/model/Message.java
@@ -2,6 +2,7 @@ package ru.nsu.fit.directors.orderservice.model;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -34,6 +35,7 @@ public class Message {
 
     private Long businessId;
 
+    @Column(name = "message_content")
     private String message;
 
     @CreationTimestamp

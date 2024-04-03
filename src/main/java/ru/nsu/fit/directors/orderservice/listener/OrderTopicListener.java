a752882c605a16cacbee64f6ab44b0bde56e2d85
Fagorym <oleg-veber@mail.ru>
2024-04-04 00:07:54 +0700

    Received business message event

--- src/main/java/ru/nsu/fit/directors/orderservice/listener/OrderTopicListener.java
+++ src/main/java/ru/nsu/fit/directors/orderservice/listener/OrderTopicListener.java
@@ -1,24 +1,35 @@
 package ru.nsu.fit.directors.orderservice.listener;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 import ru.nsu.fit.directors.orderservice.enums.OrderStatus;
+import ru.nsu.fit.directors.orderservice.event.BusinessMessageEvent;
 import ru.nsu.fit.directors.orderservice.event.OrderCancelledEvent;
 import ru.nsu.fit.directors.orderservice.event.OrderConfirmedEvent;
 import ru.nsu.fit.directors.orderservice.event.OrderCreatedEvent;
 import ru.nsu.fit.directors.orderservice.event.OrderStatusChangedEvent;
+import ru.nsu.fit.directors.orderservice.service.MessageService;
 import ru.nsu.fit.directors.orderservice.service.OrderService;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 @ParametersAreNonnullByDefault
 @KafkaListener(topics = "orderTopic")
 public class OrderTopicListener {
     private final OrderService orderService;
+    private final MessageService messageService;
+
+    @KafkaHandler
+    void handleBusinessMessage(BusinessMessageEvent businessMessageEvent) {
+        log.info("Received business message event {}", businessMessageEvent);
+        messageService.save(businessMessageEvent);
+    }
 
     @KafkaHandler
     void handleOrderCreated(OrderCreatedEvent orderCreatedEvent) {
